afac72eb5c916ed1f50064d257c3f8716b694a3d
miguel <miguelsunnata@gmail.com>
2024-09-16 10:29:05 +0200

    Initial commit

---
+++ src/main/java/com/cocaine/practice_app/blackjack_java_reboot/BlackjackJavaRebootApplication.java
@@ -0,0 +1,13 @@
+package com.cocaine.practice_app.blackjack_java_reboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackJavaRebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackjackJavaRebootApplication.class, args);
+    }
+
+}
