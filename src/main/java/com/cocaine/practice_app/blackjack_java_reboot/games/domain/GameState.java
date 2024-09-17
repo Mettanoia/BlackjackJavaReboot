2334164a3d1ac1f90f7ca24eeb9219841b9bfc44
miguel <miguelsunnata@gmail.com>
2024-09-17 10:06:01 +0200

    GameState domain entity fleshed out.

--- src/main/java/com/cocaine/practice_app/blackjack_java_reboot/games/domain/GameState.java
+++ src/main/java/com/cocaine/practice_app/blackjack_java_reboot/games/domain/GameState.java
@@ -1,4 +1,19 @@
 package com.cocaine.practice_app.blackjack_java_reboot.games.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
+
+
+@Getter
+@RequiredArgsConstructor
 final class GameState {
+
+    private final Stack<Card> deck;
+    private final List<Player> players;
+    private final Collection<Card> dealersHand;
+
 }
