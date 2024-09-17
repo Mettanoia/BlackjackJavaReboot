95dbe6fc72705c078c9816ac124fc0a33b5268af
miguel <miguelsunnata@gmail.com>
2024-09-17 13:21:40 +0200

    GameEntity added to represent games table in the database.

---
+++ src/main/java/com/cocaine/practice_app/blackjack_java_reboot/games/repository/GameEntity.java
@@ -0,0 +1,18 @@
+package com.cocaine.practice_app.blackjack_java_reboot.games.repository;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name = "games")
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+public final class GameEntity {
+
+    @Id
+    @Getter
+    private final Long Id;
+
+}
